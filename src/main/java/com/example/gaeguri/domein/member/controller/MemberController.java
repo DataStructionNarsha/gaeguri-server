51c980e7a0ad0e5742518b796d0afe4c3bd1bdfc
jiseok419 <pyosik0419@gmail.com>
2022-10-19 11:40:24 +0900

    :sparkles: :: 로그인/토큰 재발급 추가

--- src/main/java/com/example/gaeguri/domein/member/controller/MemberController.java
+++ src/main/java/com/example/gaeguri/domein/member/controller/MemberController.java
@@ -1,22 +1,27 @@
 package com.example.gaeguri.domein.member.controller;
 
+import com.example.gaeguri.domein.auth.dto.request.TokenRequestDto;
+import com.example.gaeguri.domein.auth.dto.response.TokenResponseDto;
+import com.example.gaeguri.domein.member.dto.request.UserLoginRequestDto;
 import com.example.gaeguri.domein.member.dto.request.UserSignupRequestDto;
+import com.example.gaeguri.domein.member.dto.response.UserLoginResponseDto;
 import com.example.gaeguri.domein.member.dto.response.UserSignupResponseDto;
-import com.example.gaeguri.global.service.ResponseService;
+import com.example.gaeguri.domein.member.repository.MemberRepository;
 import com.example.gaeguri.domein.member.service.SignupService;
-import com.example.gaeguri.global.Result.SingleResult;
+import com.example.gaeguri.global.Response.SingleResult;
+import com.example.gaeguri.global.exception.MemberEmailAlreadyExistsException;
+import com.example.gaeguri.global.Response.service.ResponseService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberRepository memberRepository;
     private final SignupService signupService;
     private final ResponseService responseService;
 
@@ -24,5 +29,27 @@ public class MemberController {
     public SingleResult<UserSignupResponseDto> Signup(@RequestBody UserSignupRequestDto requestDto){
         UserSignupResponseDto responseDto = signupService.signup(requestDto);
         return responseService.getSingleResult(responseDto);
+    }
+
+    @ApiOperation(value = "로컬 로그인", notes = "로컬을 통해 로그인을 진행한다.")
+    @PostMapping("/login")
+    public SingleResult<UserLoginResponseDto> login(@RequestBody UserLoginRequestDto requestDto) {
+        UserLoginResponseDto responseDto = signupService.loginMember(requestDto);
+        return responseService.getSingleResult(responseDto);
+    }
+
+    @ApiOperation(value = "토큰 재발급", notes = "Refresh Token을 통해 토큰을 재발급받는다.")
+    @PostMapping("/reissue")
+    public SingleResult<TokenResponseDto> reIssue(@RequestBody TokenRequestDto tokenRequestDto) {
+        TokenResponseDto responseDto = signupService.reIssue(tokenRequestDto);
+        return responseService.getSingleResult(responseDto);
+    }
+
+    @GetMapping("/{id}")
+    public void checkId(@PathVariable String id) {
+        if (memberRepository.findById(id).isPresent())
+            throw new MemberEmailAlreadyExistsException();
+        else
+            responseService.getSuccessResult();
     }
 }
