94e386acc0a04050da47b29f525f884a7bbd51d4
jiseok419 <pyosik0419@gmail.com>
2022-10-26 20:55:59 +0900

    :recycle: :: HttpStatus 코드 수정

--- src/main/java/com/example/gaeguri/domein/auth/Exception/ExceptionAdvice.java
+++ src/main/java/com/example/gaeguri/domein/auth/Exception/ExceptionAdvice.java
@@ -17,43 +17,43 @@ public class ExceptionAdvice {
     private final ResponseService responseService;
 
     @ExceptionHandler(MemberEmailAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public Result userEmailAlreadyExistsException() {
         return responseService.getFailureResult(-101, "이미 존재하는 닉네임입니다.");
     }
 
     @ExceptionHandler(LoginFailureException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public Result loginFailureException() {
         return responseService.getFailureResult(-102, "아이디 혹은 비밀번호가 틀립니다.");
     }
 
     @ExceptionHandler(AuthenticationEntryPointException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public Result authenticationEntryPointException() {
         return responseService.getFailureResult(-102, "인증이 필요합니다.");
     }
 
     @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public Result accessDeniedException() {
         return responseService.getFailureResult(-103, "권한이 필요합니다.");
     }
 
     @ExceptionHandler(InvalidRefreshTokenException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public Result invalidRefreshTokenException() {
         return responseService.getFailureResult(-104, "Refresh Token이 유효하지 않습니다.");
     }
 
     @ExceptionHandler(EmailNotAuthenticatedException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public Result emailAuthenticationException() {
         return responseService.getFailureResult(-105, "이메일 인증이 필요합니다.");
     }
 
     @ExceptionHandler(EmailAuthTokenNotFountException.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.OK)
     public Result emailAuthTokenNotFountException() {
         return responseService.getFailureResult(-106, "유효하지 않은 인증요청입니다.");
     }
